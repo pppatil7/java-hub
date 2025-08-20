0b2ccc69cf0695c668c062f64273d76753d16966
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-20 17:27:12 +0530

    removed braces

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -25,9 +25,7 @@ public class Main {
 
         //using interface using lambda
 
-        SayHello s = () -> {
-            System.out.println("Hello..This is using lambda");
-        };
+        SayHello s = () -> System.out.println("Hello..This is using lambda");
 
         s.sayHello();
     }
