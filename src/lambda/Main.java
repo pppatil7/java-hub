caad1f592a0a60b78040b0d039b3bb21d3b26696
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-20 17:13:37 +0530

    anonymous class

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -3,7 +3,15 @@ package lambda;
 public class Main {
 
     public static void main(String[] args) {
-        SayHello sayHello = new SayHelloImpl();
-        sayHello.sayHello();
+//        SayHello sayHello = new SayHelloImpl();
+//        sayHello.sayHello();
+
+        SayHello i = new SayHello() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello..This is anonymous class");
+            }
+        };
+        i.sayHello();
     }
 }
